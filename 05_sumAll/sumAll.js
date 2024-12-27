fb5202b3f2090ba67903bc802650ed10251b2a2c
Sourav Dey <whoamidey@outlook.com>
2024-12-27 23:44:52 +0530

    solve 1 to 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    if (!(Number.isInteger(arg1) & Number.isInteger(arg2))) {
+        return "ERROR";
+    }else if (!(arg1>0 & arg2>0)){
+        return "ERROR";
+    }else if (arg1 >= arg2){
+        let  cd = arg1-arg2+1;
+        return arg2*cd+ (cd*(cd-1))/2;
+    }else{
+        let cd = arg2-arg1+1;
+        return arg1*cd+ (cd*(cd-1))/2;
+    }
 };
 
 // Do not edit below this line
