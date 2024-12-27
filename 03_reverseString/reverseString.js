eee9378fac86fa6f4537a9730321fb8e8157740d
Sourav Dey <whoamidey@outlook.com>
2024-12-27 23:46:04 +0530

    solve 1 to 5

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let revString = "";
+    for(let i=-1 ; i>= -string.length; i=i-1){
+        revString += string.at(i);
+    };
+    return revString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...args) {
+    let newArr = [];
+    for (let item of arr){
+        if (!args.includes(item)){
+            newArr.push(item);
+            
+        };
+    }
+    return newArr;
 };
 
 // Do not edit below this line
