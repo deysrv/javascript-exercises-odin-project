b167f063553c61a521580b680209d109c1d2ab54
Sourav Dey <whoamidey@outlook.com>
2025-01-01 12:19:17 +0530

    solved all

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleDetails) {
+
+    peopleDetails.sort((a,b)=>{
+        const today = new Date()
+        const ageA = a.yearOfBirth -(a.yearOfDeath || today.getFullYear());
+        const ageB = b.yearOfBirth-(b.yearOfDeath || today.getFullYear());
+        return ageB - ageA;
+    })
+    const oldestPerson = peopleDetails.at(-1)
+
+    console.log(peopleDetails)   
+    return oldestPerson;
 
 };
 
